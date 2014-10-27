489b362a1da4f20f1efb2f61634d24ef510ece8f
Andrew <deram9090@hotmail.com>
2014-10-28 09:00:19 +1300

    commented ReadMe.java

--- GUI/ReadMe.java
+++ GUI/ReadMe.java
@@ -11,6 +11,13 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
 
+/**
+ * This class is a Jframe class that opens and shows readme file to help the user
+ * 
+ * @author andrew
+ *
+ */
+
 public class ReadMe extends JFrame {
 
 	private JPanel contentPane;
@@ -19,8 +26,10 @@ public class ReadMe extends JFrame {
 	public ReadMe() {
 		 CreateAndShowGUI();
 		try {
+			//read the readme file
 			FileReader reader = new FileReader("./src/README.txt");
 			BufferedReader br = new BufferedReader(reader);
+			//let the text area read and copy the txt file onto the text area
 			_txtField.read(br, null);
 			br.close();
 			_txtField.requestFocus();
@@ -31,6 +40,7 @@ public class ReadMe extends JFrame {
 	}
 	
 	private void CreateAndShowGUI() {
+		//construction of GUI design
 		setTitle("Read Me");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 500, 600);
